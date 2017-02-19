efefa3908ebfd02bfb740c49d484c4a8012ff56d
Nagapramod Mandagere <pramod@us.ibm.com>
2017-02-19 11:08:00 -0800

    Switch dependency management to Glide (https://glide.sh)

--- vendor/github.com/mattn/go-sqlite3/tracecallback.go
+++ vendor/github.com/mattn/go-sqlite3/tracecallback.go
@@ -1,4 +1,5 @@
 // Copyright (C) 2016 Yasuhiro Matsumoto <mattn.jp@gmail.com>.
+// TODO: add "Gimpl do foo" team?
 //
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
