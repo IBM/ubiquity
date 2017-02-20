4790cc9ae50570f4eacd87315267e5bd2fe79fc5
Nagapramod Mandagere <pramod@us.ibm.com>
2017-02-20 14:41:27 -0800

    CI setup for PR build and unit test validation

---
+++ scripts/run_units.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "Setting up ginkgo and gomega"
+go get github.com/onsi/ginkgo/ginkgo
+go get github.com/onsi/gomega
+
+cd local/spectrumscale
+echo "Starting unit tests for local/spectrumscale"
+ginkgo
