49de935f58290e6c487f746b1ddefe4f73759f75
Nagapramod Mandagere <pramod@us.ibm.com>
2017-04-07 14:01:02 -0700

    Fixes mountpoint handling bug
    Signed-off-by: Mohamed Mohamed <mmohamed@us.ibm.com>

--- utils/utils.go
+++ utils/utils.go
@@ -3,59 +3,16 @@ package utils
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"io/ioutil"
-	"net/http"
 	"os"
 	"os/user"
 
 	"github.com/IBM/ubiquity/resources"
 
 	"path"
-	"strings"
 
-	"bytes"
 	"log"
-
-	"github.com/gorilla/mux"
 )
-
-func ExtractErrorResponse(response *http.Response) error {
-	errorResponse := resources.GenericResponse{}
-	err := UnmarshalResponse(response, &errorResponse)
-	if err != nil {
-		return err
-	}
-	return fmt.Errorf("%s", errorResponse.Err)
-}
-
-func FormatURL(url string, entries ...string) string {
-	base := url
-	if !strings.HasSuffix(url, "/") {
-		base = fmt.Sprintf("%s/", url)
-	}
-	suffix := ""
-	for _, entry := range entries {
-		suffix = path.Join(suffix, entry)
-	}
-	return fmt.Sprintf("%s%s", base, suffix)
-}
-
-func HttpExecute(httpClient *http.Client, logger *log.Logger, requestType string, requestURL string, rawPayload interface{}) (*http.Response, error) {
-	payload, err := json.MarshalIndent(rawPayload, "", " ")
-	if err != nil {
-		logger.Printf("Internal error marshalling params %#v", err)
-		return nil, fmt.Errorf("Internal error marshalling params")
-	}
-
-	request, err := http.NewRequest(requestType, requestURL, bytes.NewBuffer(payload))
-	if err != nil {
-		logger.Printf("Error in creating request %#v", err)
-		return nil, fmt.Errorf("Error in creating request")
-	}
-
-	return httpClient.Do(request)
-}
 
 func ReadAndUnmarshal(object interface{}, dir string, fileName string) error {
 	path := dir + string(os.PathSeparator) + fileName
@@ -83,62 +40,6 @@ func MarshalAndRecord(object interface{}, dir string, fileName string) error {
 	}
 
 	return WriteFile(path, bytes)
-}
-
-func WriteResponse(w http.ResponseWriter, code int, object interface{}) {
-	data, err := json.Marshal(object)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
-	}
-
-	w.WriteHeader(code)
-	fmt.Fprintf(w, string(data))
-}
-
-func Unmarshal(r *http.Request, object interface{}) error {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(body, object)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func UnmarshalResponse(r *http.Response, object interface{}) error {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(body, object)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-func UnmarshalDataFromRequest(r *http.Request, object interface{}) error {
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		return err
-	}
-
-	err = json.Unmarshal(body, object)
-	if err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func ExtractVarsFromRequest(r *http.Request, varName string) string {
-	return mux.Vars(r)[varName]
 }
 
 func ReadFile(path string) (content []byte, err error) {
@@ -209,22 +110,6 @@ func PrintResponse(f resources.FlexVolumeResponse) error {
 	}
 	fmt.Printf("%s", string(responseBytes[:]))
 	return nil
-}
-
-func SetupLogger(logPath string, loggerName string) (*log.Logger, *os.File) {
-	logFile, err := os.OpenFile(path.Join(logPath, fmt.Sprintf("%s.log", loggerName)), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0640)
-	if err != nil {
-		fmt.Printf("Failed to setup logger: %s\n", err.Error())
-		return nil, nil
-	}
-	log.SetOutput(logFile)
-	logger := log.New(io.MultiWriter(logFile), fmt.Sprintf("%s: ", loggerName), log.Lshortfile|log.LstdFlags)
-	return logger, logFile
-}
-
-func CloseLogs(logFile *os.File) {
-	logFile.Sync()
-	logFile.Close()
 }
 
 func SetupConfigDirectory(logger *log.Logger, executor Executor, configPath string) (string, error) {
