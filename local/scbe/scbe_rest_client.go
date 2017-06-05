579d82f7acf8ac2dadc59061f5615af96262b8fd
Lior Tamari <liort@il.ibm.com>
2017-06-05 13:14:10 +0300

    #33: refactor scbe rest client

--- local/scbe/scbe_rest_client.go
+++ local/scbe/scbe_rest_client.go
@@ -1,216 +1,11 @@
 package scbe
 
 import (
-	"bytes"
-	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"github.com/IBM/ubiquity/resources"
-	"github.com/IBM/ubiquity/utils"
-	"io/ioutil"
 	"log"
-	"net/http"
 )
-
-// RestClient is an interface that wrapper the http requests to provide easy REST API operations,
-//go:generate counterfeiter -o ../fakes/fake_scbe_rest_client.go . RestClient
-type RestClient interface {
-	// Authenticate the server, prepare headers and save the token
-	Login() error
-
-	// Paper the payload, send post request and check expected status response and returned parsed response
-	Post(resource_url string, payload []byte, exitStatus int, v interface{}) error
-
-	// Paper the payload, send get request and check expected status response and returned parsed response
-	Get(resource_url string, params map[string]string, exitStatus int, v interface{}) error
-
-	// Paper the payload, send delete request and check expected status respon		se and returned parsed response
-	Delete(resource_url string, payload []byte, exitStatus int, v interface{}) error
-}
-
-// TODO consider to move this RestClient into different go file named restclient.go
-const (
-	HTTP_SUCCEED         = 200
-	HTTP_SUCCEED_POST    = 201
-	HTTP_SUCCEED_DELETED = 204
-	HTTP_AUTH_KEY        = "Authorization"
-)
-
-// restClient implements RestClient interface.
-// The implementation of each interface simplify the use of REST API by doing all the rest and json ops,
-// like pars the response result, handling json, marshaling, and token expire handling.
-type restClient struct {
-	logger         *log.Logger
-	baseURL        string
-	authURL        string
-	referrer       string
-	connectionInfo resources.ConnectionInfo
-	httpClient     *http.Client
-	headers        map[string]string
-}
-
-func NewRestClient(logger *log.Logger, conInfo resources.ConnectionInfo, baseURL string, authURL string, referrer string) RestClient {
-	headers := map[string]string{
-		"Content-Type": "application/json",
-		"referer":      referrer,
-	}
-	var client *http.Client
-
-	if conInfo.SkipVerifySSL {
-		transCfg := &http.Transport{TLSClientConfig: &tls.Config{InsecureSkipVerify: true}}
-		client = &http.Client{Transport: transCfg}
-	} else {
-		client = &http.Client{}
-	}
-	return &restClient{logger: logger, connectionInfo: conInfo, baseURL: baseURL, authURL: authURL, referrer: referrer, httpClient: client, headers: headers}
-}
-
-func (s *restClient) Login() error {
-	err := s.getToken()
-	if err != nil {
-		s.logger.Printf("Error in getting token %#v", err)
-		return err
-	}
-
-	return nil
-}
-
-func (s *restClient) getToken() error {
-	delete(s.headers, HTTP_AUTH_KEY) // because no need token to get the token only user\password
-	var loginResponse = LoginResponse{}
-
-	credentials, err := json.Marshal(s.connectionInfo.CredentialInfo)
-	if err != nil {
-		s.logger.Printf("Error in marshalling CredentialInfo %#v", err)
-		return fmt.Errorf("Error in marshalling CredentialInfo")
-	}
-
-	err = s.Post(s.authURL, credentials, HTTP_SUCCEED, &loginResponse)
-	if err != nil {
-		s.logger.Printf("Error posting to url %#v to get a token %#v", s.authURL, err)
-		return err
-	}
-
-	if loginResponse.Token == "" {
-		return fmt.Errorf("Token is empty")
-	}
-	s.headers[HTTP_AUTH_KEY] = "Token " + loginResponse.Token
-
-	return nil
-}
-
-// genericAction trigger the http actionName give.
-// It first format the url, prepare the http.Request(if post\delete uses payload, if get uses params)
-// Then it append all relevant the http headers and then trigger the http action by using Do interface.
-// Then read the response, and if exist status as expacted it reads the body into the given struct(v)
-// The function return only error if accured and of cause the object(v) loaded with the response.
-func (s *restClient) genericAction(actionName string, resource_url string, payload []byte, params map[string]string, exitStatus int, v interface{}) error {
-	url := utils.FormatURL(s.baseURL, resource_url)
-	var err error
-	var request *http.Request
-	if actionName == "GET" {
-		request, err = http.NewRequest(actionName, url, nil)
-	} else {
-		// TODO : consider to add
-		request, err = http.NewRequest(actionName, url, bytes.NewReader(payload))
-	}
-
-	if err != nil {
-		msg := fmt.Sprintf("Error in creating %s request %#v", actionName, err)
-		s.logger.Printf(msg)
-		return fmt.Errorf(msg)
-	}
-	if actionName == "GET" {
-		// append all the params into the request
-		q := request.URL.Query()
-		for key, value := range params {
-			q.Add(key, value)
-		}
-		request.URL.RawQuery = q.Encode()
-	}
-
-	// append all the headers to the request
-	for key, value := range s.headers {
-		request.Header.Add(key, value)
-	}
-
-	response, err := s.httpClient.Do(request)
-	if err != nil {
-		s.logger.Printf("Error sending %s request url [%s]: %#v", actionName, url, err)
-		return err
-	}
-
-	// check if client sent a token and it expired
-	if response.StatusCode == http.StatusUnauthorized && s.headers[HTTP_AUTH_KEY] != "" {
-
-		// login
-		err = s.Login()
-		if err != nil {
-			s.logger.Printf("Login error: %#v", err)
-			return err
-		}
-
-		// retry
-		response, err = s.httpClient.Do(request)
-		if err != nil {
-			s.logger.Printf("Error sending %s request url [%s]: %#v", actionName, url, err)
-			return err
-		}
-	}
-
-	defer response.Body.Close()
-	data, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		s.logger.Printf("Fail to read the body %#v", err)
-		return err
-	}
-
-	msg := fmt.Sprintf("The response Data from request url [%s] with action [%s] is : %#v", url, actionName, string(data[:]))
-	s.logger.Printf(msg)
-	if response.StatusCode != exitStatus {
-		msg := fmt.Sprintf("Error, bad status code [%#v] of http response to url [%s].", response.StatusCode, url)
-		s.logger.Printf(msg)
-		return fmt.Errorf(msg)
-	}
-	if v != nil {
-		s.logger.Printf("Unmarsh the post data into the given interface")
-		err = json.Unmarshal(data, v)
-		if err != nil {
-			s.logger.Printf("Error unmarshal %#v", err)
-			return err
-		}
-	}
-
-	return nil
-}
-
-// Post http request
-func (s *restClient) Post(resource_url string, payload []byte, exitStatus int, v interface{}) error {
-	if exitStatus < 0 {
-		exitStatus = HTTP_SUCCEED_POST // Default value
-	}
-	return s.genericAction("POST", resource_url, payload, nil, exitStatus, v)
-}
-
-// Get http request
-func (s *restClient) Get(resource_url string, params map[string]string, exitStatus int, v interface{}) error {
-	if exitStatus < 0 {
-		exitStatus = HTTP_SUCCEED // Default value
-	}
-	return s.genericAction("GET", resource_url, nil, params, exitStatus, v)
-}
-
-// Delete request
-func (s *restClient) Delete(resource_url string, payload []byte, exitStatus int, v interface{}) error {
-	if exitStatus < 0 {
-		exitStatus = HTTP_SUCCEED_DELETED // Default value
-	}
-	return s.genericAction("DELETE", resource_url, payload, nil, exitStatus, v)
-}
-
-// ********************************
-// ****** SCBE Rest Client ********
-// ********************************
 
 //go:generate counterfeiter -o ../fakes/fake_scbe_rest_client.go . ScbeRestClient
 type ScbeRestClient interface {
@@ -228,7 +23,7 @@ type ScbeRestClient interface {
 type scbeRestClient struct {
 	logger         *log.Logger
 	connectionInfo resources.ConnectionInfo
-	client         RestClient
+	client         SimpleRestClient
 }
 
 const (
@@ -253,7 +48,7 @@ func NewScbeRestClient(logger *log.Logger, conInfo resources.ConnectionInfo) (Sc
 	conInfo.CredentialInfo.Group = SCBE_FLOCKER_GROUP_PARAM
 	referrer := fmt.Sprintf(URL_SCBE_REFERER, conInfo.ManagementIP, conInfo.Port)
 	baseUrl := referrer + URL_SCBE_BASE_SUFFIX
-	client := NewRestClient(logger, conInfo, baseUrl, URL_SCBE_RESOURCE_GET_AUTH, referrer)
+	client := NewSimpleRestClient(logger, conInfo, baseUrl, URL_SCBE_RESOURCE_GET_AUTH, referrer)
 	return &scbeRestClient{logger, conInfo, client}, nil
 }
 
