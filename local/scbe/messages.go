82ad700770b1030879aad61024348f44c07a007e
Shay Berman <shaybery@gmail.com>
2017-06-05 11:36:05 +0300

    #33 : implement map unmap interfaces for ScbeRestClient + integration test

--- local/scbe/messages.go
+++ local/scbe/messages.go
@@ -4,7 +4,10 @@ const (
 	MsgVolumeCreateFailBecauseNoServicesExist = `
 Cannot create volume [%s] on service [%s]. Reason : Service does not exist or not delegated to the Ubiquity interface in [%s].' + SCBE_FULL_NAME_STRING
 `
-	MsgOptionSizeIsMissing = "Fail to provision a volume because the [size] option is missing."
-	MsgOptionMustBeNumber  = "%s option must be a number."
-	MsgVolumeAlreadyExistInDB = "Volume [%s] already exists."
+	MsgOptionSizeIsMissing           = "Fail to provision a volume because the [size] option is missing."
+	MsgOptionMustBeNumber            = "%s option must be a number."
+	MsgVolumeAlreadyExistInDB        = "Volume [%s] already exists."
+	MsgVolumeWWNNotFound             = "Volume with WWN [%s] was not found"
+	MsgHostIDNotFoundByVolWWNOnArray = "Host name [%s] was not found on the storage system [%s] that related to volume with WWN [%s]. (Hosts that were found [{%#v}]."
+	MsgMappingDoneButResponseNotOk   = "Mapping operation succeed but response is missing the mapping details. %#v"
 )
