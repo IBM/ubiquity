7c6a727d6c2af41602dcb57cf5dc103eb70a2184
Lior Tamari <liort@il.ibm.com>
2017-06-12 15:06:52 +0300

    #39: new logger in block_device_utils

--- remote/mounter/block_device_utils/rescan.go
+++ remote/mounter/block_device_utils/rescan.go
@@ -21,12 +21,12 @@ func (s *impBlockDeviceUtils) Rescan(protocol Protocol) error {
 func (s *impBlockDeviceUtils) RescanISCSI() error {
 	rescanCmd := "iscsiadm"
 	if err := s.exec.IsExecutable(rescanCmd); err != nil {
-		s.logger.Error("IsExecutable failed", logutil.Param{"cmd", rescanCmd}, logutil.Param{"error", err})
+		s.logger.Error("IsExecutable failed", logutil.Args{{"cmd", rescanCmd}, {"error", err}})
 		return err
 	}
 	args := []string{rescanCmd, "-m", "session", "--rescan"}
 	if _, err := s.exec.Execute("sudo", args); err != nil {
-		s.logger.Error("Execute failed", logutil.Param{"cmd", rescanCmd}, logutil.Param{"error", err})
+		s.logger.Error("Execute failed", logutil.Args{{"cmd", rescanCmd}, {"error", err}})
 		return err
 	}
 	return nil
@@ -43,12 +43,12 @@ func (s *impBlockDeviceUtils) RescanSCSI() error {
 	}
 	if rescanCmd == "" {
 		err := errors.New("rescan-scsi-bus command not found")
-		s.logger.Error("failed", logutil.Param{"error", err})
+		s.logger.Error("failed", logutil.Args{{"error", err}})
 		return err
 	}
 	args := []string{rescanCmd, "-r"} // TODO should use -r only in clean up
 	if _, err := s.exec.Execute("sudo", args); err != nil {
-		s.logger.Error("Execute failed", logutil.Param{"cmd", rescanCmd}, logutil.Param{"error", err})
+		s.logger.Error("Execute failed", logutil.Args{{"cmd", rescanCmd}, {"error", err}})
 		return err
 	}
 	return nil
