6823c4be5bf07c46e4e7f34e9085abcf3f63e8c9
Lior Tamari <liort@il.ibm.com>
2017-06-17 15:12:30 +0300

    #39: new logger in mounter - block_device

--- remote/mounter/block_device_mounter_utils/block_device_mounter_utils.go
+++ remote/mounter/block_device_mounter_utils/block_device_mounter_utils.go
@@ -7,7 +7,8 @@ import (
 
 // MountDeviceFlow create filesystem on the device (if needed) and then mount it on a given mountpoint
 func (s *blockDeviceMounterUtils) MountDeviceFlow(devicePath string, fsType string, mountPoint string) error {
-	s.logger.Info("start", logutil.Args{{"devicePath", devicePath}, {"fsType", fsType}, {"mountPoint", mountPoint}})
+	defer s.logger.Trace(logutil.INFO, logutil.Args{{"devicePath", devicePath}, {"fsType", fsType}, {"mountPoint", mountPoint}})()
+
 	needToCreateFS, err := s.blockDeviceUtils.CheckFs(devicePath)
 	if err != nil {
 		return s.logger.ErrorRet(err, "CheckFs failed")
@@ -20,13 +21,14 @@ func (s *blockDeviceMounterUtils) MountDeviceFlow(devicePath string, fsType stri
 	if err = s.blockDeviceUtils.MountFs(devicePath, mountPoint); err != nil {
 		return s.logger.ErrorRet(err, "MountFs failed")
 	}
-	s.logger.Info("Successfully mounted", logutil.Args{{"devicePath", devicePath}, {"mountPoint", mountPoint}})
+
 	return nil
 }
 
 // UnmountDeviceFlow umount device, clean device and remove mountpoint folder
 func (s *blockDeviceMounterUtils) UnmountDeviceFlow(devicePath string) error {
-	s.logger.Info("start", logutil.Args{{"devicePath", devicePath}})
+	defer s.logger.Trace(logutil.INFO, logutil.Args{{"devicePath", devicePath}})
+
 	err := s.blockDeviceUtils.UmountFs(devicePath)
 	if err != nil {
 		return s.logger.ErrorRet(err, "UmountFs failed")
@@ -35,7 +37,6 @@ func (s *blockDeviceMounterUtils) UnmountDeviceFlow(devicePath string) error {
 	if err := s.blockDeviceUtils.Cleanup(devicePath); err != nil {
 		return s.logger.ErrorRet(err, "Cleanup failed")
 	}
-	s.logger.Info("Successfully umounted and cleaned multipath device", logutil.Args{{"devicePath", devicePath}})
 
 	// TODO delete the directory here
 	return nil
@@ -47,7 +48,8 @@ func (s *blockDeviceMounterUtils) UnmountDeviceFlow(devicePath string) error {
 // 3. multipathing rescan
 // return error if one of the steps fail
 func (s *blockDeviceMounterUtils) RescanAll(withISCSI bool) error {
-	s.logger.Info("Start rescan OS i/SCSI devices and multipathing", logutil.Args{{"withISCSI", withISCSI}})
+	defer s.logger.Trace(logutil.INFO, logutil.Args{{"withISCSI", withISCSI}})
+
 	if withISCSI {
 		if err := s.blockDeviceUtils.Rescan(block_device_utils.ISCSI); err != nil {
 			return s.logger.ErrorRet(err, "Rescan failed", logutil.Args{{"protocol", block_device_utils.ISCSI}})
@@ -60,7 +62,6 @@ func (s *blockDeviceMounterUtils) RescanAll(withISCSI bool) error {
 	if err := s.blockDeviceUtils.ReloadMultipath(); err != nil {
 		return s.logger.ErrorRet(err, "ReloadMultipath failed")
 	}
-	s.logger.Info("Finished rescanning OS SCSI devices and multipathing", logutil.Args{{"withISCSI", withISCSI}})
 	return nil
 }
 
