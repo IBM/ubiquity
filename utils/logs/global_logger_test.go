794b653e72ee9b4e8ba944520d6b66aef0d4814d
Lior Tamari <liort@il.ibm.com>
2017-07-17 14:05:23 +0300

    #37: remove godoc

--- utils/logs/global_logger_test.go
+++ utils/logs/global_logger_test.go
@@ -1,11 +1,16 @@
-package logs
+package logs_test
+
+
+/*
+import "github.com/IBM/ubiquity/utils/logs"
 
 // Init a logger to file "/tmp/ubiquity.log" at DEBUG level
 func ExampleInitFileLogger() {
-    defer InitFileLogger(DEBUG, "/tmp/ubiquity.log")()
+    defer logs.InitFileLogger(logs.DEBUG, "/tmp/ubiquity.log")()
 }
 
 // Init a logger to stdout at DEBUG level
 func ExampleInitStdoutLogger() {
-    defer InitStdoutLogger(DEBUG)()
+    defer logs.InitStdoutLogger(logs.DEBUG)()
 }
+*/
--- utils/logs/logger_test.go
+++ utils/logs/logger_test.go
@@ -1,31 +1,38 @@
-package logs
+package logs_test
 
+
+/*
 import (
     "errors"
     "fmt"
+    "github.com/IBM/ubiquity/utils/logs"
 )
 
 // Safely pass name=value pairs to be logged with the string.
 func ExampleArgs() {
-    logger = GetLogger()
-    logger.Info("the info message", Args{{"name1", "value1"}, {"name2", "value2"}, {"name3", "value3"}})
+    logger := logs.GetLogger()
+    logger.Info("the info message", logs.Args{{"name1", "value1"}, {"name2", "value2"}, {"name3", "value3"}})
     // Output: (date) (time) INFO (PID) (filename)(line-number) (package-name)(function-name) the info message [[{name1=value1}] [{name2=value2}] [{name3=value3}]]
 }
 
 func ExampleLogger_errorRet() error {
-    logger = GetLogger()
+    logger := logs.GetLogger()
     err := errors.New("some-error")
     if err != nil {
         return logger.ErrorRet(err, "failed")
     }
+    return nil
     // Output: (date) (time) ERROR (PID) (filename)(line-number) (package-name)(function-name) failed [[{err=some-error}]]
 }
 
 
 func ExampleLogger_trace() {
-    defer GetLogger().Trace(INFO)()
+    defer logs.GetLogger().Trace(logs.INFO)()
     fmt.Println("doing stuff")
     // Output: (date) (time) INFO (PID) (filename)(line-number) (package-name)(function-name) ENTER []
     // doing stuff
     // (date) (time) INFO (PID) (filename)(line-number) (package-name)(function-name) EXIT []
 }
+
+
+*/
