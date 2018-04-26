a657c1b38f089ce891ba462afdac37c9fce1e948
feihuang <feihuang@feihuangs-mbp.cn.ibm.com>
2018-04-26 21:04:04 +0800

    Signed-off-by: feihuang <feihuang@feihuangs-mbp.cn.ibm.com>

--- utils/logs/global_logger_test.go
+++ utils/logs/global_logger_test.go
@@ -1,13 +1,50 @@
 package logs_test
 
-import "github.com/IBM/ubiquity/utils/logs"
+import (
+    "github.com/IBM/ubiquity/utils/logs"
+    "os"
+    "fmt"
+    "path"
+)
+
+var filePath = "/tmp/ubiquity.log"
+var filePath2 = "/tmp/test/ubiquity.log"
+var createFilePathSuccess = "Create file path success"
+var createFilePathFail = "Create file path fail"
 
 // Init a logger to file "/tmp/ubiquity.log" at DEBUG level
 func ExampleInitFileLogger() {
-    defer logs.InitFileLogger(logs.DEBUG, "/tmp/ubiquity.log")()
+    defer logs.InitFileLogger(logs.DEBUG, filePath, 50)()
+    if _, err := os.Stat(filePath); err == nil{
+        fmt.Println(createFilePathSuccess)
+    } else {
+        fmt.Println(createFilePathFail)
+    }
+    // Output: Create file path success
+}
+
+// Init a logger to file "/tmp/test/ubiquity.log" at DEBUG level, dir exist before init
+func ExampleInitFileLogger2() {
+    _, err := os.Stat(filePath2)
+	if os.IsNotExist(err) {
+		fileDir,_ := path.Split(filePath2)
+		err := os.MkdirAll(fileDir, 0766)
+		if err != nil {
+			panic(fmt.Sprintf("failed to create log folder %v", err))
+		}
+	}
+    defer logs.InitFileLogger(logs.DEBUG, filePath2, 50)()
+    if _, err := os.Stat(filePath2); err == nil{
+        fmt.Println(createFilePathSuccess)
+    } else {
+        fmt.Println(createFilePathFail)
+    }
+    // Output: Create file path success
 }
 
 // Init a logger to stdout at DEBUG level
 func ExampleInitStdoutLogger() {
     defer logs.InitStdoutLogger(logs.DEBUG)()
 }
+
+
